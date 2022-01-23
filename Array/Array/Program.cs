2dbb6b0ba7b6729818e3358dc9000dad318923dd
dub200586 <dkg.kapital@gmail.com>
2022-01-23 15:57:49 +0300

    ArrayHomeWork

---
+++ Array/Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Enter array number {i + 1}:");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int step;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        step = array[i];
+                        array[i] = array[j];
+                        array[j] = step;
+                    }
+                }
+                Console.Write(array[i] + " ");
+            }
+            Console.Write(array[^1]);
+        }
+    }
+}
--- Array2/Array2/Program.cs
+++ Array2/Array2/Program.cs
@@ -1,3 +1,11 @@
+﻿using System;
+
+namespace Array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             int[,] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
             int k = 0;
             for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
@@ -9,3 +17,6 @@
                 }
                 Console.WriteLine(k);
             }
+        }
+    }
+}
